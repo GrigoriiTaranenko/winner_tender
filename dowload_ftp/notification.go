7c6038b93068e4e8422277970de575732bbbd98d
User <GrigoriiTaranenko@gmail.com>
2018-04-16 01:17:35 +0300

    Make base ftp connect

--- dowload_ftp/notification.go
+++ dowload_ftp/notification.go
@@ -3,8 +3,8 @@ package dowload_ftp
 import (
 	"time"
 	"github.com/jlaffaye/ftp"
-	"fmt"
 	"errors"
+	"log"
 )
 
 const  NotificationURL = "ftp.zakupki.gov.ru:21"
@@ -12,7 +12,50 @@ const  NotificationURL = "ftp.zakupki.gov.ru:21"
 type  Notification struct {
 	FromDate time.Time
 	ToDate time.Time
+	connect bool
 	Size int
+	Client *ftp.ServerConn
+}
+
+func (this *Notification) NextDirectory(text string) error {
+	if !this.connect {
+		return errors.New("Нет подключения")
+	}
+	err := this.Client.ChangeDir(text)
+	if err != nil {
+		return err
+	}
+	cD, _ := this.Client.CurrentDir()
+	log.Printf("текущая директория %s", cD)
+	return nil
+}
+
+func (this *Notification) DownloadFile() {
+	
+}
+
+func (this *Notification) CurrentDirectory() string {
+	if !this.connect {
+		log.Printf("Нет подключения")
+		return ""
+	}
+	dir, _ := this.Client.CurrentDir()
+	return dir
+}
+
+func (this *Notification) ListClient(text string) error {
+	if !this.connect {
+		return errors.New("Нет подключения")
+	}
+	entries, err := this.Client.List(text)
+	if err != nil {
+		return errors.New(" Ошибка вывода списка клиентов " + err.Error())
+	}
+	for _, val := range entries {
+		log.Printf("%v \n", val)
+
+	}
+	return nil
 }
 
 func (this *Notification) Connect() error {
@@ -20,26 +63,12 @@ func (this *Notification) Connect() error {
 	if err != nil {
 		return errors.New(" Первая " + err.Error())
 	}
-	err = client.Login("free", "free")
+	this.Client = client
+	err = this.Client.Login("free", "free")
 	if err != nil {
 		return errors.New(" Вторая " + err.Error())
 	}
-	entries, err := client.List("/")
-	if err != nil {
-		return errors.New(" Третья " + err.Error())
-	}
-	fmt.Printf("entries = %s", entries)
+	log.Println("Подключения успешно установленно")
+	this.connect = true
 	return nil
-
-	/*	client := &http.Client{
-			Timeout: 60 * time.Second,
-		}
-		transport := &http.Transport{}
-		transport.RegisterProtocol("")
-		resp, err := client.Get(NotificationURL)
-		if err != nil {
-			log.Printf("resp err = %s \n", err.Error())
-			return
-		}
-		log.Printf("body = %s \n", resp.Body)*/
 }
--- main.go
+++ main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"github.com/GrigoriiTaranenko/winner_tender/dowload_ftp"
 	"log"
+	"bufio"
+
+	"os"
 )
 
+func outputError (err error) {
+	if err == nil {
+		return
+	}
+	log.Printf("err = %s \n", err.Error())
+}
 func main()  {
 	notification := &dowload_ftp.Notification{}
-	err := notification.Connect()
-	if err != nil {
-		log.Printf("err = %s \n", err.Error())
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		command := scanner.Text()
+		switch command {
+		case "connect":
+			outputError(notification.Connect())
+		case "curDir":
+			log.Printf("%s \n", notification.CurrentDirectory())
+		case "listData":
+			outputError(notification.ListClient(notification.CurrentDirectory()))
+		default:
+			outputError(notification.NextDirectory(command))
+		}
 	}
 	log.Println("The end")
 }
